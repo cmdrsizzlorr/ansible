fa33db1439c66095fb7f7759aa3d40c7bbfe23c5
Alan O'Flaherty <alanoflaherty@gmail.com>
2015-02-06 23:13:51 +0000

    proper structure

---
+++ roles/common/files/install-yaourt.sh
@@ -0,0 +1,13 @@
+TMP_DIR=$(mktemp -d)
+cd ${TMP_DIR}
+curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
+tar zxvf package-query.tar.gz
+cd package-query
+makepkg -si --asroot --noconfirm
+cd ..
+curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
+tar zxvf yaourt.tar.gz
+cd yaourt
+makepkg -si --asroot --noconfirm
+cd ..
+rm -rf ${TMP_DIR}
