3591503f1988651f21fab52f94510ef3dfcfb3d2
Alan O'Flaherty <alanoflaherty@gmail.com>
2015-02-07 00:04:43 +0000

    Changes!

--- roles/common/files/install-yaourt.sh
+++ roles/common/files/install-yaourt.sh
@@ -3,11 +3,11 @@ cd ${TMP_DIR}
 curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
 tar zxvf package-query.tar.gz
 cd package-query
-makepkg -si --asroot --noconfirm
+makepkg -si --noconfirm
 cd ..
 curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
 tar zxvf yaourt.tar.gz
 cd yaourt
-makepkg -si --asroot --noconfirm
+makepkg -si --noconfirm
 cd ..
 rm -rf ${TMP_DIR}
